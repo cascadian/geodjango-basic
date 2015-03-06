22bff05b55603bc165e97aaa21531bec972ad77a
Paul Currit <cascadian@users.noreply.github.com>
2015-03-06 13:29:41 -0800

    updated the database engine to be postgis

--- web/geotutorial/settings.py
+++ web/geotutorial/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'geotutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'geodjango',
         'USER': 'geo',
         'HOST': 'db',
