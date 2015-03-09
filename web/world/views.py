d4b39119f023c976befcaba07aa219d3f873b4b7
Paul Currit <cascadian@users.noreply.github.com>
2015-03-09 13:28:27 -0700

    added the initial world/index view. No integration yet with postgis

--- web/world/views.py
+++ web/world/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    context = { 'title': "A working title", 'message': "Hello, world. You're at the worlds index view"}
+    return render(request, 'world/index.html', context)
